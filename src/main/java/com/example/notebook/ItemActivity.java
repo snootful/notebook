5ee22deff822aba96193754c5386d9a1db04c0e3
Skyline <goodbyecrazy@gmail.com>
2015-11-27 22:45:14 +0800

    Initial version (no database).

--- src/main/java/com/example/notebook/ItemActivity.java
+++ src/main/java/com/example/notebook/ItemActivity.java
@@ -1,13 +1,53 @@
 package com.example.notebook;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class ItemActivity extends AppCompatActivity {
+
+    private EditText title_text, content_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+
+        title_text = (EditText) findViewById(R.id.title);
+        content_text = (EditText) findViewById(R.id.content);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        if (action.equals("com.example.notebook.EDIT_ITEM")) {
+            String titleText = intent.getStringExtra("title");
+            title_text.setText(titleText);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_add) {
+
+            Intent result = getIntent();
+            String titleText = title_text.getText().toString();
+            String contentText = content_text.getText().toString();
+            result.putExtra("title", titleText);
+            result.putExtra("content", contentText);
+            setResult(Activity.RESULT_OK, result);
+
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- src/main/java/com/example/notebook/MainActivity.java
+++ src/main/java/com/example/notebook/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.notebook;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView item_list;
+    private ArrayList<String> data = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +34,26 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent("com.example.notebook.ADD_ITEM");
+                startActivityForResult(intent, 0);
             }
         });
+
+        ListView item_list = (ListView)findViewById(R.id.item_list);
+        int layoutId = android.R.layout.simple_list_item_1;
+        adapter = new ArrayAdapter<>(this, layoutId, data);
+        item_list.setAdapter(adapter);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent("com.example.notebook.EDIT_ITEM");
+                intent.putExtra("position", position);
+                intent.putExtra("title", data.get(position));
+                startActivityForResult(intent, 1);
+            }
+        };
+        item_list.setOnItemClickListener(itemClickListener);
     }
 
     @Override
@@ -48,5 +76,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            String titleText = data.getStringExtra("title");
+            this.data.add(titleText);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
